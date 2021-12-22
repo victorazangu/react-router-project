2d9e8f9425cd7fd5961d4e48396ce53f3159a837
victor azangu <victorsazangu@gmail>
2021-12-22 03:17:43 +0300

    react router project setting up route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import NavBar from '../src/components/NavBar'
+import { BrowserRouter,Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact'
 
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+       </div>
+    </BrowserRouter>
+    
   );
 }
-
+ 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const Home = () => {
+    return (
+        <div
+            className='ui raised very padded text container segment'
+            style={{marginTop:'80px'}}
+        >
+            <h3 className='ui header'>Home</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, cumque. Magnam debitis facere adipisci beatae omnis vel recusandae earum accusantium eum commodi in sit, quia at magni expedita cumque! Quis dolore aut fugit beatae vitae. Vero suscipit, fuga magnam iste quibusdam totam rerum amet voluptas iusto. Expedita vero eius vitae beatae illo suscipit quam totam incidunt! Repellendus voluptas molestias quod quas distinctio fuga, nulla vero maxime veritatis voluptatibus nisi nihil dolor id enim? Aut quisquam exercitationem est mollitia, omnis neque nisi aliquam. Maxime eligendi soluta, quo odit minus mollitia consectetur maiores exercitationem quia ab alias ipsum placeat facilis voluptas consequatur nemo, in totam vero ut nam, impedit temporibus. Sint, laboriosam ullam. Praesentium, distinctio magnam. Commodi, eos cum. Molestias, cum eius ut rerum placeat omnis exercitationem doloribus quia voluptatibus. Aliquam commodi accusantium dicta veritatis quia optio similique facilis molestiae voluptatum, earum deserunt vel adipisci repudiandae. Velit est dolorem in at perferendis!</p>
+            
+        </div>
+    )
+}
+
+
+export default Home;
