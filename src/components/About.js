346a1ccc0b8a0c42b365a61d6fd9e72967cf8d71
victor azangu <victorsazangu@gmail>
2021-12-23 19:17:16 +0300

    working with modals

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import './modal.css';
+
 const About = () => {
     return (
-        <div
+        <div>
+            <div className='wrapper'>
+                <div className='modal'>
+                     <div className='modal-content'>
+                         <h3>I am a modal</h3>
+                     </div>
+                </div>
+            </div>
+            
+            <div
             className='ui raised very padded text container segment'
             style={{marginTop:'80px'}}
-        >
+            >
             <h3 className='ui header'>About</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, cumque. Magnam debitis facere adipisci beatae omnis vel recusandae earum accusantium eum commodi in sit, quia at magni expedita cumque! Quis dolore aut fugit beatae vitae. Vero suscipit, fuga magnam iste quibusdam totam rerum amet voluptas iusto. Expedita vero eius vitae beatae illo suscipit quam totam incidunt! Repellendus voluptas molestias quod quas distinctio fuga, nulla vero maxime veritatis voluptatibus nisi nihil dolor id enim? Aut quisquam exercitationem est mollitia, omnis neque nisi aliquam. Maxime eligendi soluta, quo odit minus mollitia consectetur maiores exercitationem quia ab alias ipsum placeat facilis voluptas consequatur nemo, in totam vero ut nam, impedit temporibus. Sint, laboriosam ullam. Praesentium, distinctio magnam. Commodi, eos cum. Molestias, cum eius ut rerum placeat omnis exercitationem doloribus quia voluptatibus. Aliquam commodi accusantium dicta veritatis quia optio similique facilis molestiae voluptatum, earum deserunt vel adipisci repudiandae. Velit est dolorem in at perferendis!</p>
             
+            </div>
         </div>
     )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Modal from './Modal'
 const Contact = (props) => {
     return (
         <div>
+            <Modal />
                 <div
             className='ui raised very padded text container segment'
             style={{marginTop:'80px'}}
