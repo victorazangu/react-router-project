308fd1022005a859c7fa76088e8e77f5dc091d89
victor azangu <victorsazangu@gmail>
2021-12-24 15:50:15 +0300

    redux-thunk

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,37 +1,48 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { deleteCard ,fetchUsers} from '../actions/cardAction'
 
 class Card extends React.Component{
 
     //state = { user: '' }
     
-    // componentDidMount() {
-    //     let user = this.props.match.params.user;
-    //     this.setState({ user })
-    // }
+    componentDidMount() {
+        this.props.fetchUsers()
+        // let user = this.props.match.params.user;
+        // this.setState({ user })
+    }
     onButtonClick = () => {
         let id = this.props.card.id
-        this.props.deletecard(id)
-        this.props.history.push('/contact')
+        this.props.deletecard(id) 
+        this.props.history.push('/contact ')
         
     }
     render() {
+        //console.log(this.props.users)
        // const { user } = this.state
         
-        const { title, body } = this.props.card;
+        const { users } = this.props;
         return (
-            <div
-                className='ui raised very padded text container segment'
-                style={{marginTop:'80px'}}
+            users.map(user => {
+                return (
+                     <div
+                        className='ui raised very padded text container segment'
+                        style={{ marginTop: '80px' }}
+                        key={user.id}
+                        
             >
-                <h3 className='ui header'>{title}</h3>
-                <p>{body}</p>
-                <button
-                    className='ui primary right floated button'
-                    onClick={this.onButtonClick}
-                >delete</button>
+                       <h3 className='ui header'>{user.name}</h3>
+                       <p>{user.email}</p>
+                       <button
+                            className='ui primary right floated button'
+                             onClick={this.onButtonClick} 
+                       >delete</button>
             
-            </div>
+                </div>
+                )
+               
+            })
+            
         ) 
     }
 }
@@ -39,14 +50,16 @@ class Card extends React.Component{
 const mapStateToprops = (state, ownProps) => {
     let title = ownProps.match.params.user
     return {
-        card: state.cards.find(card =>  card.title === title )
+        card: state.cards.find(card => card.title === title),
+        users: state.users
     }
     
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        deletecard:(id)=>{dispatch({type:'DELETE_CARD',id})}
+        deleteCard: (id) => { dispatch(deleteCard(id)) },
+        fetchUsers:()=>{dispatch(fetchUsers())}
     }
     
 }
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -4,18 +4,24 @@ const initialState = {
         { id: 2, title: 'salim', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum tempore vero accusamus minima exercitationem eos modi! Ullam at dolore delectus!.' },
         { id: 3, title: 'faith', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum tempore vero accusamus minima exercitationem eos modi! Ullam at dolore delectus!.' },
         
-    ]
+    ],
+    user:[]
 }
 const rootReducer = (state = initialState, action) => {
     
     switch (action.type) {
         case 'DELETE_CARD':
             let newCard = state.cards.filter((card) => {
-                return action.id !==card.id
+                return action.id !==  card.id
             })
             return {
                 ...state,
                 cards:newCard
+            }
+        case 'FETCH_USERS':
+            return {
+                ...state, 
+                users:action.payload
             }
         default:
             return state;
